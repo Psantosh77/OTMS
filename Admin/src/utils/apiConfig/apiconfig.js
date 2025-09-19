02d460c42198af6a5be92a27dcd12e21d223158d
Santosh Pandit <panditsantosh19971@outlook.com>
2025-09-19 19:22:28 +0530

    chnage base url

--- Admin/src/utils/apiConfig/apiconfig.js
+++ Admin/src/utils/apiConfig/apiconfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL:  'http://localhost:9000/api/',
+  baseURL:  'https://otms.onrender.com/api/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
